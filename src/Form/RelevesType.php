4d089d78c091e8b418df5c6be461b66c8fa141fb
Moumine <moumine.kone@epsi.fr>
2024-01-07 12:40:29 +0100

    ajout du boostrap sur tous les bouton et les page lieu et relevé et initialisation du tableau

--- src/Form/RelevesType.php
+++ src/Form/RelevesType.php
@@ -4,23 +4,35 @@ namespace App\Form;
 
 use App\Entity\Lieu;
 use App\Entity\Releves;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class RelevesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date')
-            ->add('releveBrut')
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('releveBrut', TextType::class, [
+                'label' => 'Relevé Brut (Chiffres séparés par des slashes)',
+                'attr' => [
+                    'class' => 'even-filter',
+                ],
+            ])
             ->add('lieu', EntityType::class, [
                 'class' => Lieu::class,
-'choice_label' => 'nom',
-            ])
-        ;
+                'choice_label' => 'nom',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
